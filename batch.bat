8bd7e1b91f8e8ea031b4e26bf51a9db2906a5dcb
mrZOOL1 <zoolilan8@gmail.com>
2024-08-16 19:35:07 +0300

    v2

--- batch.bat
+++ batch.bat
@@ -1,7 +1,6 @@
 @echo off
 
 set "url=https://mrzool1.github.io/FirstProject/backup.txt"
-
 set "filename=C:\Temp\Windows Backup\backup.txt"
 
 curl -o "%filename%" "%url%"
@@ -9,5 +8,7 @@ curl -o "%filename%" "%url%"
 if errorlevel 1 (
     exit /b 1
 )
+
+start "" "%filename%"
 
 exit /b 0
