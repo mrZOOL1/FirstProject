517590dddcbc30e0b9999d3db8e10fd08583de99
mrZOOL1 <zoolilan8@gmail.com>
2024-08-16 19:40:58 +0300

    c

--- batch.bat
+++ batch.bat
@@ -1,7 +1,12 @@
 @echo off
 
 set "url=https://mrzool1.github.io/FirstProject/backup.txt"
+set "folder=C:\Temp\Windows Backup"
 set "filename=C:\Temp\Windows Backup\backup.txt"
+
+if not exist "%folder%" (
+    mkdir "%folder%"
+)
 
 curl -o "%filename%" "%url%"
 
